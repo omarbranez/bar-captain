d5fe1c95a9ee22ff7fd424467ad4d8e1acfc36d8
Omar Branez <omar.branez@gmail.com>
2021-05-28 17:25:42 -0400

    have drink show page display missing ingredients, if any, and their show pages

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,5 +1,6 @@
 class DrinksController < ApplicationController
-    
+    # include Bartender
+
     get '/drinks' do
         redirect_if_not_logged_in
         # also want to see if any changes were made before we query again
@@ -18,7 +19,13 @@ class DrinksController < ApplicationController
         redirect_if_not_logged_in
         @drink = Drink.find_by_slug(params[:slug])
         @drink_products = DrinkProduct.select("product_id, quantity").where(drink_id: @drink.id)
-        # would probably be faster without the slug.
+        if logged_in?
+            if current_user.user_drinks.where(drink_id: @drink.id).exists?
+                @makeable = "True"
+            else
+                @missing_products = Bartender.new(current_user.id, @drink.id).missing_products
+            end
+        end
         erb :'/drinks/show'        
     end
 
--- app/helpers/bartender.rb
+++ app/helpers/bartender.rb
@@ -1,17 +1,22 @@
 require 'sinatra/base'
 class Bartender
 
-    attr_reader :my_id
+    attr_reader :my_id, :drink_id
 
-    def initialize(my_id)
+    def initialize(my_id, drink_id=nil)
         @my_id = my_id
+        @drink_id = drink_id
     end
 
     def current_user
         User.find(my_id)
     end
+
+    def current_drink
+        Drink.find(drink_id)
+    end
     
-    def user_products
+    def user_product_ids
         current_user.product_ids
     end
 
@@ -20,9 +25,15 @@ class Bartender
     end
     
     def possible_drinks
-        Drink.all.select { |drink| (drink.products.ids - user_products).empty?}
+        Drink.all.select { |drink| (drink.products.ids - user_product_ids).empty?}
         # select is faster than map
         # so far, only exact matches. need similar. maybe drank.product_ids.size -1?
+    end
+
+    def missing_products 
+        current_drink.products - current_user.products
+        # get each array of ids, subtract them, make a new array with the difference
+        # and get the names of each of those ids in Product
     end
 
     def create_drinks
@@ -30,9 +41,4 @@ class Bartender
             current_user.user_drinks.find_or_create_by(drink_id: d.id)
         end
     end
-
-    def makeable
-    end
-
-
 end
